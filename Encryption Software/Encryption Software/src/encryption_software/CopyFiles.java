5ad9ea31b45f251ac837d257a68b0d89cdbc30bd
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-06-19 19:08:40 -0400

    Add files via upload

--- Encryption Software/Encryption Software/src/encryption_software/CopyFiles.java
+++ Encryption Software/Encryption Software/src/encryption_software/CopyFiles.java
@@ -16,9 +16,26 @@ public class CopyFiles
     /*Test Main method*/
     public static void main(String[]args)
     {
-        if(args.length < 2)
+        String inputfile = "test1.txt";
+        String outputfile = "output1.txt";
+        
+        try
         {
+            InputStream inputStream = new FileInputStream(inputfile);
+            OutputStream outputStream = new FileOutputStream(outputfile);
             
+            int byteRead;
+            
+            while((byteRead = inputStream.read() ) != -1)
+            {
+                System.out.println(byteRead);
+                outputStream.write(byteRead);
+            }
+            
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
         }
     }
     
