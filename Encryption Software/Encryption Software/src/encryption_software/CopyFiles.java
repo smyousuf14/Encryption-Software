f65254f5527ac88042e0f2750ea489b45f618147
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-06-19 18:46:08 -0400

    Add files via upload

---
+++ Encryption Software/Encryption Software/src/encryption_software/CopyFiles.java
@@ -0,0 +1,25 @@
+/*
+ * Copies a file.
+ */
+package encryption_software;
+
+/*Import packages*/
+import java.io.*;
+
+/**
+ *
+ * @author Syed Yousuf
+ */
+public class CopyFiles 
+{
+    
+    /*Test Main method*/
+    public static void main(String[]args)
+    {
+        if(args.length < 2)
+        {
+            
+        }
+    }
+    
+}
